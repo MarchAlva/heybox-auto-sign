b81196915a9a977926c749f79d2665a1941391a9
WorkBuddy <workbuddy@local>
2026-08-26 21:56:47 +0800

    feat: 接入 0元抽奖盒券(heybox_roll.js)，每日签到后自动抽奖盒券

--- run_sign.bat
+++ run_sign.bat
@@ -5,5 +5,9 @@ set /p heybox_ck=<cookie.txt
 echo [%date% %time%] ==== start ==== >> sign_log.txt
 "C:\Users\March\.workbuddy\binaries\node\versions\22.22.2\node.exe" heybox_sign.js >> sign_log.txt 2>&1
 set RC=%errorlevel%
+echo [%date% %time%] sign exit=%RC% >> sign_log.txt
+"C:\Users\March\.workbuddy\binaries\node\versions\22.22.2\node.exe" heybox_roll.js >> sign_log.txt 2>&1
+set RC=%errorlevel%
+echo [%date% %time%] roll exit=%RC% >> sign_log.txt
 echo [%date% %time%] ==== end exit=%RC% ==== >> sign_log.txt
 endlocal
